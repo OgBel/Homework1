3090b806fa4b0642ca592ec2726c1c3c046dd297
OgBel <btotes@yandex.ru>
2026-03-30 17:39:20 +0500

    Доработка блоков, создание адаптивной версии

---
+++ hw-2.js
@@ -0,0 +1,50 @@
+//Задание-1
+    let a = 20;
+    alert (a);
+//Задание-2
+    let b = 2007; 
+    alert(b); 
+//Задание-3
+    const creatorName = "Brendan Eich"; 
+    alert (creatorName);
+//Задание-4
+    let x = 10;
+    let y = 2;
+    let sum = x + y;
+    let difference = x - y;
+    let product = x * y;
+    let quotient = x / y; 
+    alert(sum);
+    alert(difference);
+    alert(product);
+    alert(quotient);
+//Задание-5
+    let result = 2 ** 5;;
+    alert(result);
+//Задание-6
+    let c = 9;
+    let d = 2;
+    let remainder = c % d;
+    alert(remainder);
+//Задание-7
+    let num = 1;
+    num = num + 5;
+    num = num - 3;
+    num = num * 7;
+    num = num / 3;
+    num = num + 1;
+    num = num - 1;
+    alert(num);
+//Задание-8
+    let age = prompt("Сколько вам лет?");
+    alert(age);
+//Задание-9
+    const user = {
+   name: 'Агата',
+   age: 38,
+   isAdmin: true  
+    };
+    console.log(user);
+//Задание-10
+    const userName = prompt('Как вас зовут?');
+    alert(`Привет, ${userName}`);
